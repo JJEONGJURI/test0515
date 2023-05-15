6a8767e64d2760a8dcd20884390833ffed6882d0
GD4 <GD4@DESKTOP-CKTLCTC>
2023-05-15 17:48:53 +0900

    20230515

--- test0515/src/main/java/gdu/kr/test0515/WriteImpl.java
+++ test0515/src/main/java/gdu/kr/test0515/WriteImpl.java
@@ -1,6 +1,11 @@
 package gdu.kr.test0515;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class WriteImpl {
+	@Autowired
 	private ArticleDao dao;
 	public WriteImpl(ArticleDao dao) { //생성자
 		this.dao = dao;
